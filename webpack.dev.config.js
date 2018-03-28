46824275389cc09528060260fe9e5c8a52eccaf0
Tian Qin <tianqin@Tians-MacBook-Pro.local>
2018-03-27 23:36:12 -0700

    barebone template

---
+++ webpack.dev.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+var path = require('path');
+
+console.log(path.join(__dirname, '/index.js'));
+
+module.exports = {
+	entry: [
+		path.join(__dirname, '/index.js')
+	],
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader'
+			},
+			{
+				test: /\.less$/,
+				loaders: ["style-loader", "css-loader", "less-loader"]
+			}
+		]
+	},
+	output: {
+		path: __dirname + '/dist',
+		filename: 'bundle.js'
+	},
+	devServer: {
+		contentBase: __dirname + '/dist',
+		historyApiFallback: true
+	},
+	mode: 'development'
+}
